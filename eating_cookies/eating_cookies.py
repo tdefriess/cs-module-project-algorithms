3f7479f978b7cee73b63b8545ff9b8f47da4ed16
Tyler de Friess <tyler.defriess@gmail.com>
2020-06-18 12:45:48 -0400

    Added simple solution for small n inputs.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,8 +4,12 @@ Returns: an integer
 '''
 def eating_cookies(n):
     # Your code here
+    if n == 0:
+        return 1
+    if n < 0:
+        return 0
 
-    pass
+    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
